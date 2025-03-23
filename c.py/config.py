d366db630794eeb89ee9ee4fae40f3af08e61b78
vipabh2 <a8919832@googlemail.com>
2025-03-24 02:11:49 +0300

    Update config.py

--- c.py/config.py
+++ c.py/config.py
@@ -1,4 +1,9 @@
-import os 
-api_id = os.getenv('API_ID')      
-api_hash = os.getenv('API_HASH')  
+import os
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
+
+# تأكد من أن القيم موجودة
+if not all([api_id, api_hash, bot_token]):
+    raise ValueError("One or more environment variables are missing. Please check your API_ID, API_HASH, and BOT_TOKEN.")
