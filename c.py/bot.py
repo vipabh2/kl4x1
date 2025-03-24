cbb7260ea46b030806ba33f8a4785dd96d43c679
vipabh2 <a8919832@googlemail.com>
2025-03-24 04:33:07 +0300

    Update bot.py

--- c.py/bot.py
+++ c.py/bot.py
@@ -1,17 +1,66 @@
 from telethon import TelegramClient, events
 from config import api_id, api_hash, bot_token
-from start import start_handler  # استيراد أمر start
-from help import help_handler    # استيراد أمر help
-
-def main():
+from start import start_handler
+from help import help_handler
+async def load_plugins(folder, extfolder=None):
+    """
+    تحميل ملفات السورس
+    """
+    if extfolder:
+        path = f"{extfolder}/*.py"
+        plugin_path = extfolder
+    else:
+        path = f"kl4x1/{folder}/*.py"
+        plugin_path = f"kl4x1/{folder}"
+    files = glob.glob(path)
+    files.sort()
+    success = 0
+    failure = []
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            pluginname = shortname.replace(".py", "")
+            try:
+                if (pluginname not in Config.NO_LOAD) and (
+                    pluginname not in VPS_NOLOAD
+                ):
+                    flag = True
+                    check = 0
+                    while flag:
+                        try:
+                            load_module(
+                                pluginname,
+                                plugin_path=plugin_path,
+                            )
+                            if shortname in failure:
+                                failure.remove(shortname)
+                            success += 1
+                            break
+                        except ModuleNotFoundError as e:
+                            install_pip(e.name)
+                            check += 1
+                            if shortname not in failure:
+                                failure.append(shortname)
+                            if check > 5:
+                                break
+                else:
+                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
+            except Exception as e:
+                if shortname not in failure:
+                    failure.append(shortname)
+                os.remove(Path(f"{plugin_path}/{shortname}.py"))
+                LOGS.info(
+                    f"لم يتم تحميل {shortname} بسبب خطأ {e}\nمسار الملف {plugin_path}"
+                )
+async def main():
     # تهيئة البوت
-    client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+    ABH = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
     
-    # ربط الأوامر بالبوت
-    client.add_event_handler(start_handler)
-    client.add_event_handler(help_handler)
-    
-    client.run_until_disconnected()
+    await load_plugins("c")
+    ABH.add_event_handler(start_handler)
+    ABH.add_event_handler(help_handler)
+    ABH.run_until_disconnected()
 
 if __name__ == '__main__':
     main()
