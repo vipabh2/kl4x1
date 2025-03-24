b7421167400a5a2f53885ddb0ce6a360c874523a
vipabh2 <a8919832@googlemail.com>
2025-03-24 03:02:30 +0300

    Update and rename كشف رابط.py to كشف ايدي.py

---
+++ c.py/كشف ايدي.py
@@ -0,0 +1,26 @@
+from telethon import event
+from playwright.async_api import async_playwright # type: ignore
+@ABH.on(events.NewMessage(pattern=r'كشف ايدي (\d+)'))
+async def permalink(event):
+    global user, uid
+    uid = event.sender_id
+    user_id = event.pattern_match.group(1)
+    if not user_id:
+        await event.reply("استخدم الأمر كـ `كشف ايدي 1910015590`")
+        return
+    try:
+        user = await event.client.get_entity(int(user_id))
+    except Exception as e:
+        return await event.reply(f"لا يوجد حساب بهذا الآيدي...")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    button = KeyboardButtonCallback("تغيير الئ رابط", b"recgange")
+    await event.reply(f"⌔︙[{tag}](tg://user?id={user.id})", buttons=[button])
+@ABH.on(events.CallbackQuery(data=b"recgange"))
+async def chang(event):
+    global user, uid
+    sender_id = event.sender_id 
+    if sender_id != uid:
+        await event.answer("شلون وي الحشريين احنة \n عزيزي الامر خاص بالمرسل هوه يكدر يغير فقط😏", alert=True)
+        return
+    if uid is not None and sender_id == uid:
+        await event.edit(f"⌔︙رابط المستخدم: tg://user?id={user.id}")
